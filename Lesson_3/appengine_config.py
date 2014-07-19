71696d3a6f6caa9fef4b1a86865c727196581e91
Ryan Wise <ryanmwise1@gmail.com>
2014-07-18 19:50:36 -0700

    minor correction to appengine_config.py

--- Lesson_3/appengine_config.py
+++ Lesson_3/appengine_config.py
@@ -2,4 +2,4 @@ import sys
 import os
 
 # point appengine to third-party python libraries in the project
-sys.path.insert(2, os.path.join(os.path.abspath('.'), 'lib'))
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
